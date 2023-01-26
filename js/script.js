3f9a5627d821367a3dddf8c75d2ce16a87a18b9f
Nicko Balute <103768248+nicksXcode@users.noreply.github.com>
2023-01-26 10:44:54 +0800

    Add New Questions and Status

--- js/script.js
+++ js/script.js
@@ -6,16 +6,44 @@ let person = document.querySelector(".person");
 
 const quotes = [{
 	quote: "Who was your first crush, and what were they like?",
-	person: "Nicks Mogu"
+	person: "Nicks Mogu",
+	status: false
 }, {
 	quote: "Can you describe what your first heartbreak was like?",
-	person: "Miyang"
+	person: "Miyang",
+	status: false
 }, {
 	quote: "What was the biggest lesson from your worst breakup?",
-	person: "Nicko"
+	person: "Nicko",
+	status: false
 }, {
 	quote: "What's something you used to believe about relationships but no longer do?",
-	person: "Nicko"
+	person: "Nicko",
+	status: false
+}, {
+	quote: "What's something surprising that you were afraid of as a child?",
+	person: " ",
+	status: false
+}, {
+	quote: "What was your favorite food growing up?",
+	person: " ",
+	status: false
+}, {
+	quote: "What's your favorite meal now?",
+	person: " ",
+	status: false
+}, {
+	quote: "What was the first album you ever owned?",
+	person: " ",
+	status: false
+}, {
+	quote: "How does your life compare to how you imagined it growing up?",
+	person: " ",
+	status: false
+}, {
+	quote: "What was your dream job when you were a kid?",
+	person: " ",
+	status: false
 }];
 
 
